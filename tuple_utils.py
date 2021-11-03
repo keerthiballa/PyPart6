50452481e1599b9acbfdecf8adc904203e478c9a
Balla Keerthi <keerthiballa@gmail.com>
2021-11-02 20:53:02 -0400

    Completed Exercise 1 - adding tuples_utils.py file

--- tuple_utils.py
+++ tuple_utils.py
@@ -14,7 +14,8 @@ def tic_tac_toe_finish(board: TicTacToeBoard, pos_y: int, pos_x: int, symbol: st
     :param symbol: The symbol that should be placed in the column (X, or O)
     :return: None
     """
-    pass  # remove pass statement and implement me
+    board[pos_y][pos_x] = symbol
+    return board
 
 
 def count_instances(collection: Tuple, instance: Union[int, str]) -> int:
@@ -25,7 +26,7 @@ def count_instances(collection: Tuple, instance: Union[int, str]) -> int:
     :param instance: An item in the collection parameter
     :return: An integer.
     """
-    pass  # remove pass statement and implement me
+    return collection.count(instance)
 
 
 def print_indexes_and_entries(indexes: Iterable, entries: Iterable) -> None:
@@ -38,7 +39,8 @@ def print_indexes_and_entries(indexes: Iterable, entries: Iterable) -> None:
     :param entries: A list or tuple
     :return: None
     """
-    pass  # remove pass statement and implement me
+    for i,j in zip(indexes, entries):
+        print(f'Index: {i!s:10} Entry: {j}')
 
 
 def print_items_with_index(items: Iterable):
@@ -50,5 +52,12 @@ def print_items_with_index(items: Iterable):
     :param items: A tuple or a list
     :return: None
     """
-    pass  # remove pass statement and implement me
+
+    for i, element in enumerate(items, start=1):
+        print(f'{i}: {element}')
+
+    #j = 1
+    #for i in items:
+    #    print(f"{j}: {i}")
+    #    j = j + 1
 
